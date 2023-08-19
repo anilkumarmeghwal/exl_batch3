79824897eb3c8dfc61b7122e59fbcd291529b3a6
anilkumarmeghwal <101693397+anilkumarmeghwal@users.noreply.github.com>
2023-08-19 09:48:01 +0530

    Add files via upload

---
+++ PQ2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_questions
+{
+    internal class PQ2
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your  number");
+            int sum = 0;
+            int num = Convert.ToInt32(Console.ReadLine());
+            
+            while (num > 0)
+            {
+                sum +=num%10 ;
+                    num /= 10;
+            }
+            Console.WriteLine($"sum of all digit is : {sum}");
+           
+        }
+    }
+}
